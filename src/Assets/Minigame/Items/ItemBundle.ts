162b45f96eb451ee24585f7f4930e50432274d5c
Hyunu Jung <hyunujung0419@gmail.com>
2022-06-20 22:48:10 +0900

    Add Comment at ItemBundle.ts

--- src/Assets/Minigame/Items/ItemBundle.ts
+++ src/Assets/Minigame/Items/ItemBundle.ts
@@ -1,23 +1,35 @@
 import IItem from '../../Interfaces/IItem.js';
 
+/**
+ * 울어라 타스참마도
+ **/
 const Sword: IItem = {
   name: 'Sword',
   description: 'Normal Sword',
   price: 100,
 };
 
+/**
+ * 운터쉴드!!! 
+ **/
 const Shield: IItem = {
   name: 'Shield',
   description: 'Normal Shield',
   price: 110,
 };
 
+/**
+ * 금곡괭이 헤헤헤헤헤헿
+ **/
 const Pickaxe: IItem = {
   name: 'Pickaxe',
   description: 'Normal Pickaxe',
   price: 200,
 };
 
+/**
+ * 아이템 어셈블!!
+ **/
 const ItemBundle: IItem[] = [
   Sword,
   Shield,
