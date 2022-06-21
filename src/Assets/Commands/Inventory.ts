d33055526d93d104cd9f64c8f799507cc71e431f
Hyunu Jung <hyunujung0419@gmail.com>
2022-06-21 22:34:59 +0900

    Update Inventory

--- src/Assets/Commands/Inventory.ts
+++ src/Assets/Commands/Inventory.ts
@@ -1,4 +1,9 @@
-import { MessageEmbed, BaseCommandInteraction } from 'discord.js';
+import {
+  MessageEmbed,
+  BaseCommandInteraction,
+  CommandInteractionOptionResolver
+} from 'discord.js';
+  
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { imageurl, color } from '../Utils/EmbedConfig.js';
 import ICommand from '../Interfaces/ICommand.js';
@@ -6,15 +11,25 @@ import ICommand from '../Interfaces/ICommand.js';
 const command: ICommand = {
   Builder: new SlashCommandBuilder()
     .setName('inventory')
-    .setDescription('Check your inventory'),
+    .setDescription('Check your inventory')
+    .addBooleanOption(option =>
+      option
+        .setName('ephemeral')
+        .setDescription('Set ephemeral of message')
+        .setRequired(true)
+    ) as SlashCommandBuilder,
   SlashExecute: async (interaction: BaseCommandInteraction) => {
+    const eph = (
+      interaction.options as CommandInteractionOptionResolver
+    ).getBoolean('ephemeral');
+
     const embed = new MessageEmbed()
       .setAuthor('Gaby', imageurl)
       .setColor(color)
       .setTitle('Your Inventory')
       .setDescription('inventory');
 
-    interaction.reply({ embeds: [embed], ephemeral: true });
+    interaction.reply({ embeds: [embed], ephemeral: eph! });
   },
 };
 
