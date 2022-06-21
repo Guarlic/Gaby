00b12c4313a0063714c423b2633e5cb4fa8187bb
Hyunu Jung <hyunujung0419@gmail.com>
2022-06-21 18:44:49 +0900

    ItemBundle: edit

--- src/Assets/Minigame/Items/ItemBundle.ts
+++ src/Assets/Minigame/Items/ItemBundle.ts
@@ -1,46 +1,10 @@
 import IItem from '../../Interfaces/IItem.js';
 
-const Sword: IItem = {
-  name: 'Sword',
-  description: 'Normal Sword',
-  price: 100,
-  level: 'common',
-};
-
-const StarSword: IItem = {
-  name: 'StarSword',
-  description: 'Star Sword',
-  price: 320,
-  level: 'unique',
-};
-
-const Shield: IItem = {
-  name: 'Shield',
-  description: 'Normal Shield',
-  price: 110,
-  level: 'common',
-};
-
-const Pickaxe: IItem = {
-  name: 'Pickaxe',
-  description: 'Normal Pickaxe',
-  price: 200,
-  level: 'rare',
-};
-
-const RandomBox: IItem = {
-  name: 'RandomBox',
-  description: 'Random Box',
-  price: 150,
-  level: 'unkown',
-};
-
 const ItemBundle: IItem[] = [
-  Sword,
-  StarSword,
-  Shield,
-  Pickaxe,
-  RandomBox,
+  { name: 'Sword', description: 'Normal Sword', price: 100, level: 'common', },
+  { name: 'StarSword', description: 'Star Sword', price: 320, level: 'unique', },
+  { name: 'Shield', description: 'Normal Shield', price: 110, level: 'common', },
+  { name: 'Pickaxe', description: 'Normal Pickaxe', price: 200, level: 'rare', },
 ];
 
 export default ItemBundle;
