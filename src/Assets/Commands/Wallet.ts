00d61f4e389695f4e98bbe9dc0e35cbb6da3f297
Hyunu Jung <hyunujung0419@gmail.com>
2022-07-09 19:43:12 +0900

    v0.1.6

--- src/Assets/Commands/Wallet.ts
+++ src/Assets/Commands/Wallet.ts
@@ -1,4 +1,8 @@
-import { MessageEmbed, BaseCommandInteraction } from 'discord.js';
+import {
+  MessageEmbed,
+  BaseCommandInteraction,
+  CommandInteractionOptionResolver,
+} from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { imageurl, color } from '../Utils/EmbedConfig.js';
 import ICommand from '../Interfaces/ICommand.js';
@@ -6,12 +10,32 @@ import ICommand from '../Interfaces/ICommand.js';
 const command: ICommand = {
   Builder: new SlashCommandBuilder()
     .setName('wallet')
-    .setDescription('wallet'),
+    .setDescription('wallet')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('User who you want to check wallet')
+        .setRequired(false)
+    ) as SlashCommandBuilder,
   SlashExecute: async (interaction: BaseCommandInteraction) => {
-    const embed = new MessageEmbed()
+    const user = (
+      interaction.options as CommandInteractionOptionResolver
+    ).getUser('user');
+
+    let embed = new MessageEmbed()
       .setAuthor('Gaby', imageurl)
       .setColor(color)
-      .setDescription('Wallet');
+      .setTitle('Wallet')
+
+    if (user && user.id !== interaction.user.id) {
+      embed = embed.setDescription(
+        `${user.username}#${user.discriminator}'s wallet`
+      );
+
+      return interaction.reply({ embeds: [embed] });
+    }
+
+    embed = embed.setDescription('Your wallet');
 
     interaction.reply({ embeds: [embed] });
   },
