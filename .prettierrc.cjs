0911034db79058e0e0ade5e45dba2f9ccaf211f8
Hyunu Jung <hyunujung0419@gmail.com>
2022-07-30 10:19:13 +0900

    add .prettierrc.cjs

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  semicolon: true,
+  tabWidth: 2,
+  trailingComma: true,
+  printWidth: 80,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};
