09e0b8927384614490e7eeaf4c66b58f11f70827
Hyunu Jung <hyunujung0419@gmail.com>
2022-08-01 13:31:33 +0900

    v0.2.3

--- commit.sh
+++ commit.sh
@@ -1,7 +1,15 @@
 echo "이 프로젝트를 커밋/푸시 합니다."
-echo "커밋 메시지: $1"
+echo "커밋 메시지: $1\n"
 
 git add -A
-git commit -m "$1"
-git push origin
-git push sojak
+git commit -m $1
+
+if $1 == "v$2" :
+
+git tag "v$2" -m "Version $2"
+git push origin --tags; git push sojak --tags;
+
+done
+
+git push origin; git push sojak
+git push origin --tags; git push sojak --tags
