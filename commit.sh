0bca7b9d7921df3da9fcba702598cad20123fc38
Hyunu Jung <hyunujung0419@gmail.com>
2022-08-01 13:32:38 +0900

    v0.2.3

--- commit.sh
+++ commit.sh
@@ -4,13 +4,13 @@ echo "커밋 메시지: $1\n"
 git add -A
 git commit -m $1
 
-if ($1 == "v$2") :
-do
+if [$1 == "v$2"]
+then
 
 git tag "v$2" -m "Version $2"
 git push origin --tags; git push sojak --tags;
 
-done
+fi
 
 git push origin; git push sojak
 git push origin --tags; git push sojak --tags
