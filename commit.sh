b316566a421292ff2a7f4a1a59d57aad674e8c0a
Hyunu Jung <hyunujung0419@gmail.com>
2022-08-03 21:29:00 +0900

    fix somethings

--- commit.sh
+++
@@ -1,8 +0,0 @@
-commit_message=$1
-
-echo "이 프로젝트를 커밋/푸시 합니다."
-echo "커밋 메시지: $commit_message\n"
-
-git add -A
-git commit -m $commit_message
-git push origin; git push sojak
--- tag.sh
+++
@@ -1,6 +0,0 @@
-version=$1
-
-echo "이 프로젝트에 v$version 태그를 추가합니다.\n"
-
-git tag "v$version" -m "Version $version"
-git push origin --tags; git push sojak --tags
