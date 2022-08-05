245fc77929e0e62d1bc286a6fbb80dc4ef9ee44f
Hyunu Jung <hyunujung0419@gmail.com>
2022-08-05 12:24:15 +0900

    Profile command - add target

--- src/Assets/Commands/Profile.ts
+++ src/Assets/Commands/Profile.ts
@@ -1,6 +1,7 @@
 import {
   MessageAttachment,
-  BaseCommandInteraction
+  BaseCommandInteraction,
+  CommandInteractionOptionResolver
 } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { readFile } from 'fs/promises';
@@ -12,12 +13,29 @@ import ICommand from '../Interfaces/ICommand.js';
 const command: ICommand = {
   Builder: new SlashCommandBuilder()
     .setName('profile')
-    .setDescription('your profile'),
+    .setDescription('your profile')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('select user')
+    ) as SlashCommandBuilder,
   SlashExecute: async (interaction: BaseCommandInteraction) => {
-    const data = await UserModel.findOne({ id: interaction.user?.id });
+    const options = interaction.options as CommandInteractionOptionResolver;
+    
+    const otarget = options.getUser('user');
+    const target = otarget ? otarget : interaction.user;
 
-    if (!data) {
-      interaction.reply('가입을 해주고 찾아와라');
+    const userdata = await UserModel.findOne({ id: interaction.user?.id });
+    const targetdata = await UserModel.findOne({ id: target!.id });
+
+    if (target!.bot) {
+      interaction.reply('그 유저는 봇입니다10000?');
+
+      return;
+    }
+
+    if (!userdata || !targetdata) {
+      interaction.reply('가입..');
 
       return;
     }
@@ -39,7 +57,7 @@ const command: ICommand = {
     context.fillStyle = '#0xBDBDBD';
 
     context.fillText(
-      `${interaction.user.username}#${interaction.user.discriminator}`,
+      `${target!.username}#${target!.discriminator}`,
       canvas.width / 2.5,
       canvas.height / 1.8
     );
@@ -47,7 +65,7 @@ const command: ICommand = {
     context.font = '40px Maplestory';
 
     context.fillText(
-      `Lv.${data.level}`,
+      `Lv.${targetdata.level}`,
       canvas.width / 2.5,
       canvas.height / 1.4
     );
@@ -55,13 +73,13 @@ const command: ICommand = {
     context.font = '30px Maplestory';
 
     context.fillText(
-      `${data.exp} / ${data.level * 100}xp`,
+      `${targetdata.exp} / ${targetdata.level * 100}xp`,
       canvas.width / 2.5,
       canvas.height / 1.2
     );
 
     context.fillText(
-      `id: ${interaction.user.id}`,
+      `id: ${target!.id}`,
       canvas.width / 2.5,
       canvas.height / 3
     );
@@ -71,7 +89,7 @@ const command: ICommand = {
     context.closePath();
     context.clip();
 
-    const { body } = await request(interaction.user.displayAvatarURL({ format: 'jpg' }));
+    const { body } = await request(target!.displayAvatarURL({ format: 'jpg' }));
     const avatar = new Canvas.Image();
     avatar.src = Buffer.from(await body.arrayBuffer());
 
@@ -79,7 +97,7 @@ const command: ICommand = {
 
     const attachment = new MessageAttachment(canvas.toBuffer('image/png'));
 
-    interaction.reply({ content: '!Your Profile!', files: [attachment] });
+    interaction.reply({ files: [attachment] });
   },
 };
 
