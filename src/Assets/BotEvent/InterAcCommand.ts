f2f04127f9267f50587f43df807b77b599abc758
Hyunu Jung <hyunujung0419@gmail.com>
2022-08-15 19:08:37 +0900

    auto exp

--- src/Assets/BotEvent/InterAcCommand.ts
+++ src/Assets/BotEvent/InterAcCommand.ts
@@ -1,4 +1,5 @@
 import { BaseCommandInteraction } from 'discord.js';
+import { UserModel } from '../DataBase/UserSchema.js';
 import CommandBundle from '../Commands/CommandBundle.js';
 import logger from '../Utils/Logger.js';
 
@@ -15,6 +16,31 @@ At Server <${interaction.guild?.name}>
 User [${interaction.user.username}]
 executed command: ${interaction.commandName}`
   );
+
+  const data = await UserModel.findOne({ id: interaction.user.id });
+
+  const ignorelist = [
+    'profile',
+    'wallet',
+    'inventory',
+  ];
+
+  if (data && !ignorelist.find(value => value === interaction.commandName)) {
+    const xplist = [ 1, 3, 5, 7, 9, 10 ];
+    const xp = xplist[Math.floor(Math.random() * xplist.length)];
+
+    data.exp += xp;
+
+    if (data.exp >= data.level * 100) {
+      data.exp -= data.level * 100;
+      data.level++;
+      interaction.channel?.send(
+        `<@${interaction.user?.id}>, Levelup! (${data.level - 1} -> ${data.level})`
+      );
+    }
+
+    data.save();
+  }
 
   command.SlashExecute(interaction);
 }
